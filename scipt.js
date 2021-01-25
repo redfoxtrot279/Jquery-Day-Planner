84713b14ce0f632fb9ff532935e0ff20c1f78f0a
Redfoxtrot279 <redfoxtrot279@gmail.com>
2021-01-25 14:25:55 -0600

    added psuedo code for needed help

---
+++ scipt.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    //create timeblocks
+    //add times to blocks
+    //add text area for blocks
+    //add save button for each timeblock
+    //change color of timeblock based on time of day
+    
+}
